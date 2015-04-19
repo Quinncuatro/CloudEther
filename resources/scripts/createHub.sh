84cffa531d48ac8df86050f00b25836e4c9a21a2
Matt <matthew.prahl@mymail.champlain.edu>
2015-04-18 23:24:15 -0400

    Fixed Scripts

--- resources/scripts/createHub.sh
+++ resources/scripts/createHub.sh
@@ -3,12 +3,12 @@ if [ -n "$1" ];
 then
         echo "INFO: Creating $1 Hub"
         HubPassword=`date +%s | sha256sum | base64 | head -c 32`
+        /usr/local/vpnserver/vpncmd localhost:5555 /SERVER /PASSWORD:champlain /CMD HubCreate $1 /PASSWORD:$HubPassword &> /dev/null
         echo "INFO: $1 Created"
         echo "Password: $HubPassword"
         exit 0;
-else
 
+else
         echo "ERROR: No Hub Name Specified"
         exit 1;
 fi
-
--- resources/scripts/deleteHub.sh
+++ resources/scripts/deleteHub.sh
@@ -2,10 +2,11 @@
 if [ -n "$1" ];
 then
         echo "INFO: Removing $1"
+        /usr/local/vpnserver/vpncmd localhost:5555 /SERVER /PASSWORD:champlain /CMD HubDelete $1 $> /dev/null
         echo "INFO: Removed $1"
         exit 0;
+
 else
         echo "ERROR: No Hub Name Specified"
         exit 1;
 fi
-
