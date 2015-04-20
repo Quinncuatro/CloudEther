063a0f9721c55c4bfb9f65b9fe3633a660f609ed
Skamadix <skamadix@me.com>
2015-04-20 12:05:30 -0400

    Added error checking to bash script & created status check script

---
+++ resources/scripts/vpnStatus.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source config
+
+name=
+
+function serverStatus
+{
+
+portStatus=`nc -z 0.0.0.0 5555; echo $?`
+if [ $portStatus -eq 0 ]
+then
+	echo "Service Is Online"
+	exit 3
+else
+	echo "Service Is Offline"
+	exit 4
+fi
+
+}
+
+function getHub
+{
+
+hub=`/usr/local/vpnserver/vpncmd localhost:5555 /SERVER /PASSWORD:$softetherPass /CMD Hub $name | grep 'The Virtual Hub' | grep $name`
+
+if [ -z "$hub" ]
+then
+	echo "Hub Does Not Exist"
+	exit 5
+else
+	echo "Hub Exists"
+	exit 6
+fi
+
+}
+
+
+while getopts ":g:s" OPTION
+do
+     case $OPTION in
+         s)
+		serverStatus
+		;;
+         g)
+		name=$OPTARG
+		getHub
+
+             ;;
+     esac
+done
+
+
